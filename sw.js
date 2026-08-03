abae7eb13da47536837989d3476d4052ea886189
lucky <lux_senarak@hotmail.com>
2026-08-03 22:00:32 +0700

    ปรับการบันทึกและสต๊อกสินค้า

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Taii-shop-v3'; //
+const CACHE_NAME = 'Taii-shop-v3.1'; //
 const SHELL_FILES = ['./', './index.html', './manifest.json']; //[cite: 2]
 
 self.addEventListener('install', (event) => {
