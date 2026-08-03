06cd7e8d91822fde5c26659949f1b602bf78a3af
lucky <lux_senarak@hotmail.com>
2026-08-03 20:08:37 +0700

    .

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Taii-shop-v2'; //
+const CACHE_NAME = 'Taii-shop-v3'; //
 const SHELL_FILES = ['./', './index.html', './manifest.json']; //[cite: 2]
 
 self.addEventListener('install', (event) => {
