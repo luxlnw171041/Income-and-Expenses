b47c6507668a1390b4a2795108df36f97aea2f7c
lucky <lux_senarak@hotmail.com>
2026-08-20 19:11:25 +0700

    แก้บัค

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Taii-shop-v4.1.2'; //
+const CACHE_NAME = 'Taii-shop-v4.2'; //
 const SHELL_FILES = ['./', './index.html', './manifest.json']; //[cite: 2]
 
 self.addEventListener('install', (event) => {
